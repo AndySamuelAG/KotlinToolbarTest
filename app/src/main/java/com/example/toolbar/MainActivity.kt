40da0764e9213f72e494d3278b94d110bf596fe2
Andy Alvarado <AndyAlvarado1997@gmail.com>
2020-02-17 18:58:42 -0500

    Init

---
+++ app/src/main/java/com/example/toolbar/MainActivity.kt
@@ -0,0 +1,72 @@
+package com.example.toolbar
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Button
+import android.widget.SearchView
+import android.widget.Toast
+import androidx.appcompat.widget.Toolbar
+
+class MainActivity : AppCompatActivity() {
+
+    var toolbar: Toolbar? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        toolbar = findViewById<Toolbar>(R.id.toolbar)
+        toolbar?.setTitle(R.string.app_name)
+        setSupportActionBar(toolbar)
+
+        val bPantallaDos = findViewById<Button>(R.id.bPantallaDos)
+        bPantallaDos.setOnClickListener {
+            val intent = Intent(this, PantallaDos::class.java)
+            startActivity(intent)
+        }
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+        val itemBusqueda = menu?.findItem(R.id.busqueda)
+        var vistaBusqueda = itemBusqueda?.actionView as SearchView
+
+        vistaBusqueda.queryHint = "Escribe tu nombre"
+        vistaBusqueda.setOnQueryTextFocusChangeListener { v, hasFocus ->
+            Log.d("LISTENERFOCUS", hasFocus.toString())
+        }
+
+        vistaBusqueda.setOnQueryTextListener(object: SearchView.OnQueryTextListener{
+            override fun onQueryTextChange(newText: String?): Boolean {
+                Log.d("OnQueryTextChange", newText)
+                return true
+            }
+
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                Log.d("OnQueryTextSubmit", query)
+                return true
+            }
+        })
+
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item?.itemId){
+            R.id.bFav -> {
+                Toast.makeText(this, "Elemento añadido como favorito", Toast.LENGTH_SHORT).show()
+                return true
+            }
+
+            else -> {
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+}
